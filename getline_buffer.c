77017a2ae896127347e93560c7f1fd7e6279939d
meddizzle316 <meddizzle316@gmail.com>
2023-11-30 13:05:28 -0600

    added null terminator at end of new string

--- getline_buffer.c
+++ getline_buffer.c
@@ -4,22 +4,19 @@ char *getline_buffer(char *buffer, size_t buff_size)
 {
 	int characters;
 	char *getline_buffer;
-
+	int length;
+	
+	length = strlen(buffer);
 	characters = getline(&buffer, &buff_size, stdin);
-	if (characters == -1)
+	getline_buffer = (char *)malloc(sizeof(char) * length);
+	if (getline_buffer == NULL || characters == -1)
 	{
-		perror("characters is -1\n");
-		free(buffer);
-		return (NULL);
-	}
-	getline_buffer = (char *)malloc(sizeof(char) * (strlen(buffer)));
-	if (getline_buffer == NULL)
-	{
-		perror("getline_buffer is NULL");
+		perror("getline_buffer is NULL or characters = -1");
 		free(buffer);
 		free(getline_buffer);
 		return (NULL);
 	}
-	strncpy(getline_buffer, buffer, (strlen(buffer) - 1));
+	strncpy(getline_buffer, buffer, length - 1);
+	getline_buffer[length] = '\0';
 	return (getline_buffer);
 }
