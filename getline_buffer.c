d056b130d8e5482e1fdad1a55b444d24c6f4ac5f
meddizzle316 <meddizzle316@gmail.com>
2023-11-30 13:54:03 -0600

    changed newline removal seems like it produces less memory errors

--- getline_buffer.c
+++ getline_buffer.c
@@ -5,10 +5,11 @@ char *getline_buffer(char *buffer, size_t buff_size)
 	int characters;
 	char *getline_buffer;
 	int length;
-	
+	int i;
+
 	length = strlen(buffer);
 	characters = getline(&buffer, &buff_size, stdin);
-	getline_buffer = (char *)malloc(sizeof(char) * length);
+	getline_buffer = (char *)malloc(sizeof(char) * length + 1);
 	if (getline_buffer == NULL || characters == -1)
 	{
 		perror("getline_buffer is NULL or characters = -1");
@@ -16,7 +17,13 @@ char *getline_buffer(char *buffer, size_t buff_size)
 		free(getline_buffer);
 		return (NULL);
 	}
-	strncpy(getline_buffer, buffer, length - 1);
-	getline_buffer[length] = '\0';
+	strcpy(getline_buffer, buffer);
+	for (i = 0; getline_buffer[i]; i++)
+	{
+		if (getline_buffer[i] == '\n')
+		{
+			getline_buffer[i] = '\0';
+		}
+	}
 	return (getline_buffer);
 }
