cdb95a1d69e571f6aeec6f04b3b9b396ac52aaf2
meddizzle316 <meddizzle316@gmail.com>
2023-12-05 23:00:47 -0600

    fixed memory errors when taking input from terminal I think

--- getline_buffer.c
+++ getline_buffer.c
@@ -10,11 +10,12 @@ char *getline_buffer(int flag)
 	char *getline_buffer = NULL, *buffer = NULL;
 	size_t buff_size = 0;
 
-	getline_buffer = malloc(sizeof(char *) * 5);
-	getline_buffer[0] = '\0';
 	if (flag == 1)
 	{
-		while ((getline(&buffer, &buff_size, stdin)) != -1)
+	getline_buffer = malloc(sizeof(char *) * 5);
+	getline_buffer[0] = '\0';
+		
+	while ((getline(&buffer, &buff_size, stdin)) != -1)
 		{
 			getline_buffer = realloc(getline_buffer, strlen(getline_buffer) + strlen(buffer) + 1);
 			strcat(getline_buffer, buffer);
--- simple_shell_3.c
+++ simple_shell_3.c
@@ -82,7 +82,6 @@ int main (void)
 					free_array(tokenArray);
 					exit(127);
 				}
-		
 			}
 			if (value == -1)
 			{
