4d1ce4a90101c246c76f636bb27a6cd155490970
meddizzle316 <meddizzle316@gmail.com>
2023-12-07 10:48:57 -0600

    betty checks for free_all.c

--- free_all.c
+++ free_all.c
@@ -5,10 +5,10 @@
  * @arg: for freeing, abbreviation for tokenized_array
  * @path_exists: tells if tokenArray exists
  * @path: for freeing, abbreviation for tokenArray
- * @exit_status: exit status
+ * @status: exit status
  * Return: void
  */
-void free_all(char** tmp, char** arg, int path_exists, char** path, int status)
+void free_all(char **tmp, char **arg, int path_exists, char **path, int status)
 {
 	free_array(tmp);
 	free_array(arg);
